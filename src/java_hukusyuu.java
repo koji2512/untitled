860489d17e7a24e2de6dec744072b9f4233b9c7b
kojimahayato <241029@std.hi-joho.ac.jp>
2025-05-14 10:36:38 +0900

    復習問題1.2.3.4

---
+++ src/java_hukusyuu.java
@@ -0,0 +1,35 @@
+public class java_hukusyuu {
+    public static void main(String[] args){
+        //復習問題１
+        System.out.print("こんにちは。");
+        System.out.println("今日の天気は"+"\t晴れです。");
+        System.out.print("今日は一日\n洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.print("文字は',"+"文字列は\"で囲みましょう。");
+
+        System.out.println("\n");
+        //復習問題2
+        System.out.println(20+30+"50"+7+1);
+        System.out.println(90+5-4+"5"+(5 - 10));
+        System.out.println(90+5 - 5+"50");
+
+        System.out.println("\n");
+        //復習問題3
+        System.out.println(10-7*2+"7"+5*-10);
+        System.out.println(3*2*3+7+"0"+5+3);
+        System.out.println("7"+3*2+5+6/2);
+
+        System.out.println("\n");
+        //復習問題4
+        int number = 5;
+        String str = 88 +"88";
+       double PI=3.141592;
+        PI=3.14;
+        float circ = 3.14F;
+        long a = 10000000000L;
+        boolean flg = true;
+        byte b=35;
+        String num = "Ox735";
+        char c = 'a';
+    }
+}
