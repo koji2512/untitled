24247b93aca8017724fbe885f8b94faeca1e67d4
kojimahayato <241029@std.hi-joho.ac.jp>
2025-05-21 10:32:37 +0900

    復習問題1.2.3.4,5,6,7,8

---
+++ src/Hukusyuu_two.java
@@ -0,0 +1,2 @@
+public class Hukusyuu_two {
+}
--- src/Java_hukusyuu.java
+++ src/Java_hukusyuu.java
@@ -1,35 +1,81 @@
 public class Java_hukusyuu {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //復習問題１
         System.out.print("こんにちは。");
-        System.out.println("今日の天気は"+"\t晴れです。");
+        System.out.println("今日の天気は" + "\t晴れです。");
         System.out.print("今日は一日\n洗濯日和です。");
         System.out.println("外に干しても大丈夫そうです。");
-        System.out.print("文字は',"+"文字列は\"で囲みましょう。");
+        System.out.print("文字は'," + "文字列は\"で囲みましょう。");
 
         System.out.println("\n");
         //復習問題2
-        System.out.println(20+30+"50"+7+1);
-        System.out.println(90+5-4+"5"+(5 - 10));
-        System.out.println(90+5 - 5+"50");
+        System.out.println(20 + 30 + "50" + 7 + 1);
+        System.out.println(90 + 5 - 4 + "5" + (5 - 10));
+        System.out.println(90 + 5 - 5 + "50");
 
         System.out.println("\n");
         //復習問題3
-        System.out.println(10-7*2+"7"+5*-10);
-        System.out.println(3*2*3+7+"0"+5+3);
-        System.out.println("7"+3*2+5+6/2);
+        System.out.println(10 - 7 * 2 + "7" + 5 * -10);
+        System.out.println(3 * 2 * 3 + 7 + "0" + 5 + 3);
+        System.out.println("7" + 3 * 2 + 5 + 6 / 2);
 
         System.out.println("\n");
         //復習問題4
         int number = 5;
-        String str = 88 +"88";
-       double PI=3.141592;
-        PI=3.14;
+        String str = 88 + "88";
+        double PI = 3.141592;
+        PI = 3.14;
         float circ = 3.14F;
         long a = 10000000000L;
         boolean flg = true;
-        byte b=35;
+        byte b = 35;
         String num = "Ox735";
         char c = 'a';
+
+        System.out.println("\n");
+        //復習問題5
+        int A=10;int B=20;int C=10;
+        System.out.println(A==B);
+        System.out.println(A==C);
+        System.out.println(A !=B);
+        System.out.println(A !=C);
+        System.out.println(A<B);
+        System.out.println(A<=C);
+        System.out.println(A>B);
+        System.out.println(A>=C);
+        System.out.println(A+10==B);
+        System.out.println(C+10==B+-5);
+
+        System.out.println("\n");
+        //復習問題6
+        System.out.println(105+5);
+        System.out.println(10-2.5);
+        System.out.println(100/3);
+        System.out.println(10*3.0);
+        System.out.println(50.0/5);
+
+        System.out.println("\n");
+        //復習問題7
+        System.out.println(4*5*2+10+"3"+6+2);
+        System.out.println("10"+4*3+-5+8/4);
+        System.out.println(15-3*2+"5"+3*7);
+        System.out.println("5"+6*4+7+10/2);
+        System.out.println((10+5)*3+"-2"+4+-5);
+        System.out.println(4*(5+2)-10+"3"+6+2);
+        System.out.println("10"+(4*3-5)+8/4);
+        System.out.println((15+-3)*(2+5)+(3+7));
+        System.out.println("5"+(6-4+7)+10/2);
+        System.out.println((3+4)*2+"20"+12/(4+2));
+
+        System.out.println("\n");
+        //復習問題8
+       A=10;
+        double bb = A;
+        A=(int)bb;
+        A=10000;
+        bb=10000;
+        bb=a;
+        double f=5.94;
+        short s=10;
     }
 }
