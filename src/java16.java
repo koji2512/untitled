97728a922544f58b2a9346ce53d9f91682dc4792
kojimahayato <241029@std.hi-joho.ac.jp>
2025-05-26 12:28:43 +0900

    16.17.18

---
+++ src/java16.java
@@ -0,0 +1,13 @@
+public class java16 {
+    public static void main(String[] args) {
+        int num = 4;
+        int sum=0;
+        int i= 1;
+        while(i<=num){
+            sum+=i;
+            i++;
+        }
+        System.out.println("結果"+sum);
+    }
+
+}
---
+++ src/java18.java
@@ -0,0 +1,12 @@
+public class java18 {
+    public static void main(String[] args) {
+        int num=4;
+        int sum=0;
+        int i=1;
+        while(i<=num){
+            sum+=i;
+            i+=2;
+        }
+        System.out.println("結果"+sum);
+    }
+}
