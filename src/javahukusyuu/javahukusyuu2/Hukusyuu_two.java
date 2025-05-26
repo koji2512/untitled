3aea6afb0449ae1d140a89c3a439f6e4bb9da042
kojimahayato <241029@std.hi-joho.ac.jp>
2025-05-26 12:11:31 +0900

    復習問題２－１

--- src/javahukusyuu/javahukusyuu2/Hukusyuu_two.java
+++ src/javahukusyuu/javahukusyuu2/Hukusyuu_two.java
@@ -1,4 +1,13 @@
 package javahukusyuu.javahukusyuu2;
-
-public class Hukusyuu_two {
+public class Hukusyuu_two{
+    public static void main(String[] args) {
+        int num = 4;
+        if(num%2==0) {
+            System.out.println("偶数");
+        }else if(num%2==1) {
+            System.out.println("奇数");
+        }else{
+            System.out.println("無効な入力です。");
+        }
+    }
 }
