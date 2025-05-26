43d5bb8314b8d06060b2e61cd67f3c0dae69461c
kojimahayato <241029@std.hi-joho.ac.jp>
2025-05-26 15:16:42 +0900

    復習問題3

---
+++ src/javahukusyuu/javahukusyuu3/Java29.java
@@ -0,0 +1,16 @@
+package javahukusyuu.javahukusyuu3;
+
+public class Java29 {
+    public static void main(String[] args) {
+        KeyInput key=new KeyInput();
+        System.out.print("宛先を入力＞");
+        String to=key.readString();
+        System.out.print("件名を入力＞");
+        String subject=key.readString();
+        System.out.print("本文を入力＞");
+        String body=key.readString();
+        System.out.println(to+"に以下のメールを送信しました。");
+        System.out.println("件名："+subject);
+        System.out.println("本文："+body);
+    }
+}
